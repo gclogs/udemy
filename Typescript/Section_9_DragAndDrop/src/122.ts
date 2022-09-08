a65ca764f2be2192c35db084ae4f6bdc07f9a27c
GyeongChan <woig9874@naver.com>
2022-09-08 14:26:37 +0000

    122. DOM Element Selection & OOP Rendering

--- Typescript/Section_9_DragAndDrop/src/122.ts
+++ Typescript/Section_9_DragAndDrop/src/122.ts
@@ -8,9 +8,13 @@ class ProjectInput122 {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true);
+    // 콘텐츠 렌더링에 필요한 프로퍼티
+    const importedNode = document.importNode(
+      this.templateElement.content, // 템플릿의 내용을 참조
+      true  // 깊은 복사를 이용하여 가져오기
+      );
 
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement; // 삽입하려는 노드를 가리키는 프로퍼티
     this.attach();
   }
 
