728e4412231d4be364504d6622680d307867ffa0
GyeongChan <woig9874@naver.com>
2022-09-08 14:43:54 +0000

    -

--- Typescript/Section_9_DragAndDrop/src/app.ts
+++ Typescript/Section_9_DragAndDrop/src/app.ts
@@ -147,12 +147,13 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
     this.hostElement = document.getElementById(hostElementId)! as T;
 
+    
     const importedNode = document.importNode(
-      this.templateElement.content,
-      true
+      this.templateElement.content, 
+      true 
     );
 
-    this.element = importedNode.firstElementChild as U;
+    this.element = importedNode.firstElementChild as U; 
     if(newElementId) {
       this.element.id = newElementId;
     }
