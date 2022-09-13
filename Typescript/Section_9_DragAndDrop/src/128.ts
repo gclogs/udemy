bc00e8d5c494c005deecbcca0a7df3888aa71741
GyeongChan <woig9874@naver.com>
2022-09-13 11:05:41 +0000

    128. Managing Application State with Singletons

--- Typescript/Section_9_DragAndDrop/src/128.ts
+++ Typescript/Section_9_DragAndDrop/src/128.ts
@@ -1,6 +1,70 @@
 // 128. Managing Application State with Singletons
 
 /**
+ * #128 개선 항목
+ * 1. 타입을 명확히 하기
+ * 2. 중복 출력 방지
+ */
+
+// #128 Project State Management
+class ProjectState128 {
+  // #128 상태 관리 솔루션에 초점을 두고자 any 타입으로 선언
+  private projects: any[] = [];
+  private static instance: ProjectState128;
+  // #128 내용이 변경될 때마다 함수를 호출해야함
+  private listeners: any[] = []; 
+
+  /**
+   * #128 싱글톤 생성자
+   */
+  private constructor() {
+
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState128();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+
+  /**
+   * #128 버튼 액션
+   * 
+   * Add Project 버튼을 클릭할 때마다 프로젝트 추가
+   */
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(), // #128 난수로 고유 id 생성
+      title: title,
+      description: description,
+      people: numOfPeople
+    }
+    this.projects.push(newProject);
+    // #128 새로운 프로젝트를 추가하거나 변동이 있을 때마다 모든 리스너 함수를 호출
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+/**
+ * #128
+ * 
+ * 싱글톤으로 생성된 ProjectState
+ * 하나의 상태 관리 객체를 가질 수 있으므로
+ * 동일한 객체로 항상 어디서든, 작업할 수 있게됨.
+ * 
+ * 싱글톤 개념을 확실하게 이해해야겠음.
+ */
+const projectstate = ProjectState128.getInstance();
+
+/**
  * #124 
  * 
  * AutoBind 데코레이터를 사용하여 bind 메서드의 재사용성을 대폭 높여주고,
@@ -26,7 +90,7 @@
 }
 
 /**
- * #128
+ * #127
  * 
  * 검증하는 인터페이스를 만들어 validate 함수에 할당
  * 옵셔널 연산자를 사용해 프로퍼티가 꼭 필요하지 않음을 알림
@@ -42,16 +106,16 @@ interface Validatable128 {
 }
 
 /**
- * #128
+ * #127
  * 
  * Validatable128 인터페이스를 정적 타입으로 받고,
  * 이를 기반으로 인수로 들어온 값을 검증하는 함수
  * @param validatableInput 값
  * @returns boolean
  */
-function validate(validatableInput: Validatable128) {
+function validate127(validatableInput: Validatable128) {
   let isValid = true;
-  if (validatableInput.required) { // #128 required가 true고, 공란이 아니라면 true
+  if (validatableInput.required) { // #127 required가 true고, 공란이 아니라면 true
     isValid = isValid && validatableInput.value.toString().trim().length !== 0
   }
   if (validatableInput.minLength != null && typeof validatableInput.value == 'string') {
@@ -75,10 +139,12 @@ class ProjectList128 {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
+  assignedProjects: any[];
   
   constructor(private type: 'active' | 'finished') { 
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.assignedProjects = [];
 
     const importedNode = document.importNode(
       this.templateElement.content,
@@ -88,10 +154,26 @@ class ProjectList128 {
     this.element = importedNode.firstElementChild as HTMLElement; 
     this.element.id = `${type}-projects`;
 
+
+    projectState.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+      this.renerProjects();
+    });
+
     this.rederContent();
     this.attach();
    }
 
+
+   private renerProjects() {
+    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) { // #128 this.assignedProject 항목을 모두 살펴봄
+      const listItem = document.createElement('li') 
+      listItem.textContent = prjItem.title;
+      listEl?.appendChild(listItem) // #128 모든 항목에 <li>  목록을 추가
+    }
+   }
+   
   private rederContent() {
     const listId = `${this.type}-projects-list`;
     this.element.querySelector('ul')!.id = listId;
@@ -207,12 +289,13 @@ class ProjectInput128 {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
+      projectstate.addProject(title, desc, people);
       this.clearInput();
     }
   }
 
   /**
-   * #128
+   * #127
    * Input 요소의 내용을 초기화
    */
   private clearInput() {
